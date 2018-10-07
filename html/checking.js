796ca818954d5c77e53755caae95c2ebc43b4b72
Sydney Jenkins <sydneydanielle28@gmail.com>
2018-10-07 11:22:41 -0700

    Display info TEST

---
+++ html/checking.js
@@ -0,0 +1,11 @@
+(function(){
+
+    function button_link($scope) {
+
+        $scope.Locationbtn = new Locationbtn();
+        $scope.Destinationbtn = new Destinationbtn();
+
+        console.log(Locationbtn)
+        console.log(Destinationbtn)
+    }
+})();
